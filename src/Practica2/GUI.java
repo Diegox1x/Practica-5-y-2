6963a277067ea86761b5c0bd34f084326ae20e0e
Diegox1x <diegojaramillo581@gmail.com>
2026-08-27 18:20:07 -0600

    Menus correctos de la practica

--- src/Practica2/GUI.java
+++ src/Practica2/GUI.java
@@ -4,55 +4,65 @@ import javax.swing.*;
 public class GUI extends JFrame {
     
     // Objetos de la ventana
-    JMenuBar barraMenu;
-    JMenu menuArchivo, menuEditar, menuReporte, menuSalir;
-    JMenuItem itemNuevo, itemConsultar, itemEliminar, itemModificar, itemPdf, itemTxt;
-
+    JMenuBar barra;
+    JMenu archivo, editar, reporte, salir;
+    JMenuItem nuevo, consultar, eliminar, modificar, archivoPdf, archivoTxt;
+    JLabel fondo;
+    
     // Propiedades de la ventana
     public GUI() {
-        setTitle("Practica2");
+        setTitle("Práctica 2");
         setSize(380, 280);
         setLayout(null);
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(this);
+        //setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         componentes();
-        setVisible(true);
+        setVisible(true);  
     }
-
+    
     public void componentes() {
-        // barra principal
-        barraMenu = new JMenuBar();
-
-        // menús principales (pestañas)
-        menuArchivo = new JMenu("Archivo");
-        menuEditar = new JMenu("Editar");
-        menuReporte = new JMenu("Reporte");
-        menuSalir = new JMenu("Salir");
-
-        // opciones internas (items)
-        itemNuevo = new JMenuItem("Nuevo");
-        itemConsultar = new JMenuItem("Consultar");
-        itemEliminar = new JMenuItem("Eliminar");
-        itemModificar = new JMenuItem("Modificar");
-        itemPdf = new JMenuItem("Archivo PDF");
-        itemTxt = new JMenuItem("Archivo TXT");
-
-        // respectivos menús
-        menuArchivo.add(itemNuevo);
-        menuArchivo.add(itemTxt);
-        menuArchivo.add(itemPdf);
-
-        menuEditar.add(itemConsultar);
-        menuEditar.add(itemModificar);
-        menuEditar.add(itemEliminar);
-
-        // barra principal
-        barraMenu.add(menuArchivo);
-        barraMenu.add(menuEditar);
-        barraMenu.add(menuReporte);
-        barraMenu.add(menuSalir);
-
-        // barra en la ventana
-        setJMenuBar(barraMenu);
+        // 1. Barra principal
+        barra = new JMenuBar();
+        
+        // 2. Menús principales de la barra
+        archivo = new JMenu("Archivo");
+        editar = new JMenu("Editar");
+        reporte = new JMenu("Reporte");
+        salir = new JMenu("Salir");
+        
+        // 3. Opciones internas (Items)
+        nuevo = new JMenuItem("Nuevo");
+        consultar = new JMenuItem("Consultar");
+        eliminar = new JMenuItem("Eliminar");
+        
+        modificar = new JMenuItem("Modificar");
+        
+        archivoPdf = new JMenuItem("Archivo PDF");
+        archivoTxt = new JMenuItem("Archivo txt");
+        
+        // 4. Agregar items a sus menús correspondientes
+        archivo.add(nuevo);
+        archivo.add(consultar);
+        archivo.add(eliminar);
+        
+        editar.add(modificar);
+        
+        reporte.add(archivoPdf);
+        reporte.add(archivoTxt);
+        
+        // 5. Agregar los menús a la barra principal
+        barra.add(archivo);
+        barra.add(editar);
+        barra.add(reporte);
+        barra.add(salir);
+        
+        // 6. Asignar la barra a la ventana
+        setJMenuBar(barra);
+        
+        // 7. Etiqueta central de Fondo
+        fondo = new JLabel("Fondo", SwingConstants.CENTER);
+        fondo.setBounds(0, 0, 365, 220);
+        add(fondo);
     }
 }
